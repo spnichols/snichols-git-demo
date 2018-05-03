e71ed20216c2064f7e72feb9922d1dca0234fd33
snichols <stephen.nichols@slalom.com>
2018-05-03 03:12:40 -0700

    Update to testAppMain

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello Lunch and Learn";
+    private final String message = "Hello Lunch and Learn!";
 
     public App() {}
 
